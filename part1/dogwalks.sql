bb324b6f58a2a1f509864b67b7abe650cff3ff72
Jasonchen0726 <unknown@student.adelaide.edu.au>
2025-06-19 23:56:21 +0000

    [automatic commit] 2025-06-19 23:56:21 UTC 220.244.20.70

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -13,7 +13,7 @@ CREATE TABLE Users (
 CREATE TABLE Dogs (
     dog_id INT AUTO_INCREMENT PRIMARY KEY,
     owner_id INT NOT NULL,
-    name VARCHAR(50) NOT NULL,
+    name VARCHAR(50) NOT NULL
     size ENUM('small', 'medium', 'large') NOT NULL,
     FOREIGN KEY (owner_id) REFERENCES Users(user_id)
 );
