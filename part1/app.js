b3a4058eff304b4a3012c92404dcfb9a31562775
Jasonchen0726 <unknown@student.adelaide.edu.au>
2025-06-20 14:55:53 +0000

    [automatic commit] 2025-06-20 14:55:53 UTC 220.244.20.70

--- part1/app.js
+++ part1/app.js
@@ -38,7 +38,7 @@ let db;
 
         });
         await db.execute(`
-      CREATE TABLE IF NOT EXISTS Users (
+        CREATE TABLE IF NOT EXISTS Users (
         user_id INT AUTO_INCREMENT PRIMARY KEY,
         username VARCHAR(50) UNIQUE NOT NULL,
         email VARCHAR(100) UNIQUE NOT NULL,
@@ -49,7 +49,7 @@ let db;
     `);
 
         await db.execute(`
-      CREATE TABLE IF NOT EXISTS Dogs (
+        CREATE TABLE IF NOT EXISTS Dogs (
         dog_id INT AUTO_INCREMENT PRIMARY KEY,
         owner_id INT NOT NULL,
         name VARCHAR(50) NOT NULL,
@@ -59,15 +59,15 @@ let db;
     `);
 
         await db.execute(`
-    CREATE TABLE IF NOT EXISTS WalkRequests(
-    request_id INT AUTO_INCREMENT PRIMARY KEY,
-    dog_id INT NOT NULL,
-    requested_time DATETIME NOT NULL,
-    duration_minutes INT NOT NULL,
-    location VARCHAR(255) NOT NULL,
-    status ENUM('open', 'accepted', 'completed', 'cancelled') DEFAULT 'open',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (dog_id) REFERENCES Dogs(dog_id)
+        CREATE TABLE IF NOT EXISTS WalkRequests(
+        request_id INT AUTO_INCREMENT PRIMARY KEY,
+        dog_id INT NOT NULL,
+        requested_time DATETIME NOT NULL,
+        duration_minutes INT NOT NULL,
+        location VARCHAR(255) NOT NULL,
+        status ENUM('open', 'accepted', 'completed', 'cancelled') DEFAULT 'open',
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (dog_id) REFERENCES Dogs(dog_id)
     )
     `);
 
