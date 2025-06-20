0627e1daea6a0254b455564307a66845f022a761
Jasonchen0726 <unknown@student.adelaide.edu.au>
2025-06-20 02:10:11 +0000

    [automatic commit] 2025-06-20 02:10:11 UTC 220.244.20.70

--- part1/app.js
+++ part1/app.js
@@ -97,6 +97,9 @@ let db;
     `);
 
 
+await db.execute
+
+
         await db.execute(`
       INSERT INTO Users (username, email, password_hash, role) VALUES
       ('alice123', 'alice@example.com', 'hashed123', 'owner'),
