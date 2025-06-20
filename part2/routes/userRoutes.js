33b04ef0fb99d3ec0d958d25d5cef0b0b0bf4db6
Jasonchen0726 <unknown@student.adelaide.edu.au>
2025-06-20 08:30:22 +0000

    [automatic commit] 2025-06-20 08:30:22 UTC 220.244.20.70

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -71,7 +71,9 @@ router.post('/login', async (req, res) => {
 
 router.post('/logout', (req, res) => {
   req.session.destroy(err => {
-    if (err) return res.status(500).json({ error: 'Logout failed' });
+    if (err) {
+      return res.status(500).json({ error: 'Logout failed' });
+    }
     res.json({ message: 'Logged out' });
   });
 });
