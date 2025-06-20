8ac2ca410212633810dd7cb1dbe35a2a50326fb9
Jasonchen0726 <unknown@student.adelaide.edu.au>
2025-06-20 01:13:17 +0000

    [automatic commit] 2025-06-20 01:13:17 UTC 220.244.20.70

--- part1/app.js
+++ part1/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mysql=require()
+var mysql=require('mysql2/promise');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,6 +14,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
