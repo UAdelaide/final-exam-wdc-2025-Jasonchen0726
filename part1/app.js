7768728dc653d75da7f37e84cdd0dc9039b0faa1
Jasonchen0726 <unknown@student.adelaide.edu.au>
2025-06-20 02:11:38 +0000

    [automatic commit] 2025-06-20 02:11:38 UTC 220.244.20.70

--- part1/app.js
+++ part1/app.js
@@ -99,7 +99,8 @@ let db;
 
 await db.execute(`DELETE FROM Users`);
 await db.execute(`DELETE FROM Dogs`);
-await db.execute(`DELETE FROM W)
+await db.execute(``)
+await db.execute(`DELETE FROM WalkRatings`);
 
 
         await db.execute(`
