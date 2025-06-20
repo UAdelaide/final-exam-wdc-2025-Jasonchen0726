ba564481a6b0e8ddb1f4306201b00505001a7969
Jasonchen0726 <unknown@student.adelaide.edu.au>
2025-06-20 12:00:41 +0000

    [automatic commit] 2025-06-20 12:00:41 UTC 220.244.20.70

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -94,7 +94,7 @@ router.get('/mydogs',async(req,res) => {
 
 
 
-router.get('/api/dogs', async (req, res) => {
+router.get('/getdogs', async (req, res) => {
     try {
 const [rows] = await db.query('SELECT dog_id, name, size FROM Dogs');
         res.json(rows);
