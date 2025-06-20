0a89c48554377272d7ac33382ee9bd9e430edd4c
Jasonchen0726 <unknown@student.adelaide.edu.au>
2025-06-20 02:27:19 +0000

    [automatic commit] 2025-06-20 02:27:19 UTC 220.244.20.70

--- part1/app.js
+++ part1/app.js
@@ -172,7 +172,9 @@ app.get('/api/walkers/summary',async(req,res)=>{
 
             (SELECT COUNT(*) FROM WalkRequests req JOIN WalkRatings r ON r.request_id =req.request_id
             WHERE r.walker_id =u.user_id AND req.status ='completed) AS completed_walks
-            FROM Users u LEFT JOIN WalkRatings wr ON wr.walker_id = u
+            FROM Users u LEFT JOIN WalkRatings wr ON wr.walker_id = u.user_id
+            WHERE u.role='walker'
+            group by 
 
 
             `);
