815fcce2d4edcae5ccc39bc4e57109a8d738ccd0
Jasonchen0726 <unknown@student.adelaide.edu.au>
2025-06-20 02:20:40 +0000

    [automatic commit] 2025-06-20 02:20:40 UTC 220.244.20.70

--- part1/app.js
+++ part1/app.js
@@ -167,7 +167,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
 
 app.get('/api/walkers/summary',async(req,res)=>{
     try{
-        const[rows]=await db.execute(`SELECT u.username AS walker_username,  `);
+        const[rows]=await db.execute(`SELECT u.username AS walker_username, COUNT `);
 
         res.json(rows);
     } catch(err){
