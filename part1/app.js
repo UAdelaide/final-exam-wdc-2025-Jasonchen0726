9ab6501c37aa5c57461d260e90694cbe5e12d5f2
Jasonchen0726 <unknown@student.adelaide.edu.au>
2025-06-20 02:40:15 +0000

    [automatic commit] 2025-06-20 02:40:15 UTC 220.244.20.70

--- part1/app.js
+++ part1/app.js
@@ -137,7 +137,7 @@ let db;
 
 await db.execute(`INSERT INTO WalkRatings (request_id,walker_id,owner_id,rating,comments) VALUES
 
-    (SELECT request_id )
+    (SELECT request_id FROM WalkRequests WHERE dog_id=(SELECT dog))
 
 
 
