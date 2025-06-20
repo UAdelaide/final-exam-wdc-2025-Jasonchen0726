16afcf1887588902cd79baa09cfdbea67907f47a
Jasonchen0726 <unknown@student.adelaide.edu.au>
2025-06-20 01:31:04 +0000

    [automatic commit] 2025-06-20 01:31:03 UTC 220.244.20.70

--- part1/app.js
+++ part1/app.js
@@ -109,10 +109,7 @@ let db;
 
 
     await db.execute(`
-      INSERT INTO Users (username, email, password_hash, role) VALUES
-      ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-      ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-      ('carol123', 'carol@example.com', 'hashed789', 'owner')
+      INSERT INTO Dogs (owner_id,name,size)
     `)
 
 
