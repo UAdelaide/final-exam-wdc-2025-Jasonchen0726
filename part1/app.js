f6772e83d5ece34ae022f7f39b56199ed6334933
Jasonchen0726 <unknown@student.adelaide.edu.au>
2025-06-20 01:44:25 +0000

    [automatic commit] 2025-06-20 01:44:25 UTC 220.244.20.70

--- part1/app.js
+++ part1/app.js
@@ -6,8 +6,6 @@ var mysql = require('mysql2/promise');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const { asyncWrapProviders } = require('async_hooks');
-
 var app = express();
 
 app.use(logger('dev'));
