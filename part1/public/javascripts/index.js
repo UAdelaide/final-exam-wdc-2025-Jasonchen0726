ff3f3f020760de02fcbfbaccdd9384cf64478651
Jasonchen0726 <unknown@student.adelaide.edu.au>
2025-06-20 07:05:03 +0000

    [automatic commit] 2025-06-20 07:05:03 UTC 220.244.20.70

--- part1/public/javascripts/index.js
+++ part1/public/javascripts/index.js
@@ -12,7 +12,9 @@ createApp({
             try{
                 const res=await fetch('https://dog.ceo/api/breeds/image/random');
                 const data=await res.json();
-                this dogImage=data.message;
+                this.dogImage = data.message;
+            } catch(err){
+                
             }
         }
     }
