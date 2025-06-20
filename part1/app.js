80d94f9f648801055a639a17d0c30bfafbc3f561
Jasonchen0726 <unknown@student.adelaide.edu.au>
2025-06-20 02:31:38 +0000

    [automatic commit] 2025-06-20 02:31:38 UTC 220.244.20.70

--- part1/app.js
+++ part1/app.js
@@ -118,7 +118,10 @@ let db;
         await db.execute(`
       INSERT INTO Dogs (owner_id,name,size) VALUES
       ((SELECT user_id FROM Users WHERE username='alice123'), 'Max', 'medium'),
-      ((SELECT user_id FROM Users WHERE username='carol123'), 'Bella', 'small')
+      ((SELECT user_id FROM Users WHERE username='carol123'), 'Bella', 'small'),
+      ((SELECT user_id FROM Users WHERE username = 'jason123'), 'Toby', 'large'),
+((SELECT user_id FROM Users WHERE username = 'jack123'), 'Jedi', 'medium')
+
     `);
 
 
