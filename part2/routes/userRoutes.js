c51a1a995d0e28738ccd54183865affd711bb1be
Jasonchen0726 <unknown@student.adelaide.edu.au>
2025-06-20 11:45:33 +0000

    [automatic commit] 2025-06-20 11:45:33 UTC 220.244.20.70

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -94,4 +94,18 @@ router.get('/mydogs',async(req,res) => {
 
 
 
+router.get('/api/dogs', async (req, res) => {
+    try {
+        const [rows] = await db.execute(`
+      SELECT d.name AS dog_name, d.size, u.username AS owner_username
+      FROM Dogs d JOIN Users u ON d.owner_id = u.user_id
+    `);
+        res.json(rows);
+    } catch (err) {
+        res.status(500).json({ error: 'api failed' });
+    }
+});
+
+
+
 module.exports = router;
