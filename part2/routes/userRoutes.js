6493f8f6f4f582148c83138e602ac5e84177d8fc
Jasonchen0726 <unknown@student.adelaide.edu.au>
2025-06-20 07:43:01 +0000

    [automatic commit] 2025-06-20 07:43:01 UTC 220.244.20.70

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,16 +49,16 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-req.session.user={
-  id:rows[0].user_id,
-  username:rows[0].username,
-  role:rows[0].role
+    req.session.user = {
+      id: rows[0].user_id,
+      username: rows[0].username,
+      role: rows[0].role
 
-};
+    };
 
 
 
-    res.json({ message: 'Login successful', user: rows[0] });
+    res.json({ message: 'Login successful', user: req.session.user });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
   }
