d05a629cbc807aa8e805b008a26ca8125a9a79fc
Jasonchen0726 <unknown@student.adelaide.edu.au>
2025-06-20 07:42:38 +0000

    [automatic commit] 2025-06-20 07:42:38 UTC 220.244.20.70

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -51,9 +51,10 @@ router.post('/login', async (req, res) => {
 
 req.session.user={
   id:rows[0].user_id,
-  username:rows[0].username
+  username:rows[0].username,
+  role:rows[0].role
 
-}
+};
 
 
 
