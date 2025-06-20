2b80b87d9d055ffded93dc7c1a9420769b6eded1
Jasonchen0726 <unknown@student.adelaide.edu.au>
2025-06-20 16:59:22 +0000

    [automatic commit] 2025-06-20 16:59:22 UTC ;; communications error to 208.67.222.222#53: timed out 220.244.20.70

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -47,8 +47,8 @@ router.get('/me', (req, res) => {
 // it get the data from the request
 // then it run sql commands to check within the database
 // if the request body is matched within the database
-//  it will get the row data including fields like user_id
-// then apply the data in the session and send the response
+// it will get the row data including fields like user_id....
+// then store the data in the session and send the response
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
