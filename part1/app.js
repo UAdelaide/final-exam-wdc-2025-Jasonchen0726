4c4786491bdd9bdfec56b673cc56053e6d63e9d1
Jasonchen0726 <unknown@student.adelaide.edu.au>
2025-06-20 01:54:55 +0000

    [automatic commit] 2025-06-20 01:54:55 UTC 220.244.20.70

--- part1/app.js
+++ part1/app.js
@@ -138,7 +138,7 @@ app.get('/api/dogs', async (req, res) => {
 
 app.get('/api/walkrequests/open', async(req,res)=>{
     try{
-        const[rows]=await db.execute(` SELECT wr.
+        const[rows]=await db.execute(` SELECT wr.request_id, 
 
              `
 
