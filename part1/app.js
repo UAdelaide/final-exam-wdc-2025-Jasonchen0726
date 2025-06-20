28a13e102b6d207ff5ede8d85d4a0def53b4dfca
Jasonchen0726 <unknown@student.adelaide.edu.au>
2025-06-20 14:57:36 +0000

    [automatic commit] 2025-06-20 14:57:36 UTC 220.244.20.70

--- part1/app.js
+++ part1/app.js
@@ -143,11 +143,8 @@ let db;
         (SELECT request_id FROM WalkRequests WHERE dog_id=(SELECT dog_id FROM Dogs WHERE name='Bob')),
         (SELECT user_id FROM Users WHERE username='bobwalker'),
         (SELECT user_id FROM Users WHERE username = 'bobwalker'),
-    5,'no comments'
-
-        )
-
-`);
+        5,'no comments')
+    `);
 
 
 
